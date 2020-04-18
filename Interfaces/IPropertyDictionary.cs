68fe923f614ddd9841f80094a87bab760b2c6e75
anloffredo <anloffredo@massageenvy.com>
2020-04-18 01:17:34 -0700

    Auto Commit

---
+++ Interfaces/IPropertyDictionary.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Penguin.SystemExtensions.Abstractions.Interfaces
+{
+    public interface IPropertyDictionary<TKey, TReturn>
+    {
+        TReturn this[TKey key] { get; }
+
+        int Count { get; }
+        IEnumerable<TKey> Keys { get; }
+        IEnumerable<TReturn> Values { get; }
+
+        bool ContainsKey(TKey key);
+        IEnumerator<KeyValuePair<TKey, TReturn>> GetEnumerator();
+        bool TryGetValue(TKey key, out TReturn value);
+    }
+}
