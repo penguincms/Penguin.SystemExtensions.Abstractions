36f9a5df515197b7d0acdf24da3012681a43b3dc
anloffredo <anloffredo@massageenvy.com>
2020-05-03 10:56:50 -0700

    Auto Commit

--- Interfaces/IPropertyDictionary.cs
+++ Interfaces/IPropertyDictionary.cs
@@ -4,14 +4,15 @@ namespace Penguin.SystemExtensions.Abstractions.Interfaces
 {
     public interface IPropertyDictionary<TKey, TReturn>
     {
-        TReturn this[TKey key] { get; }
-
         int Count { get; }
         IEnumerable<TKey> Keys { get; }
         IEnumerable<TReturn> Values { get; }
+        TReturn this[TKey key] { get; }
 
         bool ContainsKey(TKey key);
+
         IEnumerator<KeyValuePair<TKey, TReturn>> GetEnumerator();
+
         bool TryGetValue(TKey key, out TReturn value);
     }
 }
